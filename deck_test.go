fa7183d52e24acc60918a1e9a2cbeef9125db608
Melvin Toni <melvin.working@gmail.com>
2021-11-10 13:04:36 +0700

    add test case

--- deck_test.go
+++ deck_test.go
@@ -8,4 +8,12 @@ func TestNewDeck(t *testing.T) {
 	if len(d) != 16 {
 		t.Errorf("Expected deck length of 16, but got %v", len(d))
 	}
+
+	// if d[0] != "Ace of Spades" {
+	// 	t.Errorf("Expected deck of Ace of Spades, but got %v", d[0])
+	// }
+
+	// if d[len(d)-1] != "Ace of Spades" {
+	// 	t.Errorf("Expected deck of Ace of Spades, but got %v", d[len(d)-1])
+	// }
 }
